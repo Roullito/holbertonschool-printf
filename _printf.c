ff76234cffb0c56e7d918d3a29a2b2bdcb207dbe
Roullito <jmoleins@laposte.net>
2025-03-24 15:57:32 +0100

    add _printf function that read the string passed by the user in _printf function. search % and read the char after. Then call the function that manage the char associate

--- _printf.c
+++ _printf.c
@@ -1,14 +1,40 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...)
 {
 	int i;
+	int count = 0;
+	va_list args;
+	int (*func)(va_list);
+
+	va_start(args, format);
 
 	for(i = 0; format[i] != '\0'; i++)
 	{
-		write(1, &format[i], 1);
+		if (format[i] == '%' && format[i + 1])
+		{
+			i++;
+			func = get_func(format[i]);
+
+			if (func)
+			{
+				count += func(args);
+			}
+
+			else
+			{
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
+
+		}
+			write(1, &format[i], 1);
+			count++;
 	}
 
-	return (i - 1);
+	va_end(args);
+	return (count);
 }

29f70a03c386dc0217bafc02b96c6f55345608e0
Roullito <jmoleins@laposte.net>
2025-03-24 16:01:49 +0100

    add prototype for every functions used

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
+int (*get_func(char c))(va_list);
+int c_type(va_list args);
+int s_type(va_list args);
+int p_type(va_list args);
 
 #endif

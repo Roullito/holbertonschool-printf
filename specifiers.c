81baf05861d6d7d3f37e37c2e4fa6d5f2e40b395
Roullito <jmoleins@laposte.net>
2025-03-25 11:50:08 +0100

    add specifiers d and i

--- specifiers.c
+++ specifiers.c
@@ -41,3 +41,47 @@ int p_type(va_list args)
 	(void)args;
 	return (write(1, "%", 1));
 }
+
+/**
+ * d_type - Handles the %d format specifier for numbers between 0 and 99
+ * @args: list of variadic arguments (expects an int)
+ * Return: The original integer (not the number of characters printed)
+ */
+
+int d_type(va_list args)
+{
+	int d;
+	int unite = 0;
+	int dizaine = 0;
+
+	d = va_arg(args, int);
+	unite = ((d % 10) + '0');
+	dizaine = ((d / 10) + '0');
+
+	(write(1, &dizaine, 1));
+	(write(1, &unite, 1));
+
+	return (d);
+}
+
+/**
+ * i_type - Handles the %i format specifier for numbers between 0 and 99
+ * @args: list of variadic arguments (expects an int)
+ * Return: The original integer (not the number of characters printed)
+ */
+
+int i_type(va_list args)
+{
+	int i;
+	int unite = 0;
+	int dizaine = 0;
+
+	i = va_arg(args, int);
+	unite = ((i % 10) + '0');
+	dizaine = ((i / 10) + '0');
+
+	(write(1, &dizaine, 1));
+	(write(1, &unite, 1));
+
+	return (i);
+}

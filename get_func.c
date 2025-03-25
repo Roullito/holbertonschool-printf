348441acbc072c40213515ceeca134a931fe80e3
f-qrm <qerimifjolla7@gmail.com>
2025-03-25 10:11:40 +0100

    betty style

--- get_func.c
+++ get_func.c
@@ -1,11 +1,19 @@
 #include "main.h"
 #include <stddef.h>
 #include <stdarg.h>
+/**
+ * get_func - Selects the appropriate function based on the format specifier.
+ *@c: The format specifier character.
+ *Return: NULL
+ */
 
 int (*get_func(char c))(va_list)
 {
-	if (c == 'c') return (c_type);
-	if (c == 's') return (s_type);
-	if (c == '%') return (p_type);
+	if (c == 'c')
+		return (c_type);
+	if (c == 's')
+		return (s_type);
+	if (c == '%')
+		return (p_type);
 	return (NULL);
 }
--- specifiers.c
+++ specifiers.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdarg.h>
-
+/**
+ * c_type - Function to handle the 'c' format specifier (character)
+ * @args: The list of arguments passed to the function
+ * Return: The number of bytes written (1 byte for the character)
+ */
 int c_type(va_list args)
 {
 	char c;
@@ -9,13 +13,17 @@ int c_type(va_list args)
 	c = va_arg(args, int);
 	return (write(1, &c, 1));
 }
-
+/**
+ * s_type - Function to handle the 's' format specifier (string)
+ * @args: The list of arguments passed to the function
+ * Return: The number of characters written
+ */
 int s_type(va_list args)
 {
 	char *s = va_arg(args, char *);
 	int i = 0;
 
-	if(!s)
+	if (!s)
 		s = "(null)";
 
 	while (s[i])
@@ -23,7 +31,11 @@ int s_type(va_list args)
 
 	return (i);
 }
-
+/**
+ * p_type - Function to handle the '%' format specifier
+ * @args: The list of arguments passed to the function (not used here)
+ * Return: The number of bytes written, which is 1 (for the '%' character)
+ */
 int p_type(va_list args)
 {
 	(void)args;
